4b65caefc725161490a31b36aab999ee6cacd7cc
Engineering <Engineering@308-14.cls-edu.charlottelatin.org>
2022-09-09 10:13:04 -0400

    Second commit

--- executing.java
+++ executing.java
@@ -2,10 +2,10 @@ class Main{
     public static void main(String[] args_){
         Person Bob = new Person(178.0, 50,"Bob","Blond");
         Person Carlos = new Person(190.0, 19,"Carlos","White");
-        Person Robert = new Person(160.0, 12,"Robert","Dark");
+        Person Person = new Person(160.0, 12,"Robert","Dark");
         HotelRoom House = new HotelRoom(1);
-        House.checkin(Bob);
-        House.checkin(Carlos);
+        House.checkIn(Bob);
+        House.checkIn(Carlos);
         House.checkCapacity();
     }
 }
