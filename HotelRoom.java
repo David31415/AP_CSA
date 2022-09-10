a0347c8a9d01e80d74ad736a9f8ecab67137028b
David Tian <David_Tian@TIANdeMacBook-Pro.local>
2022-09-09 22:27:02 -0400

    commit 2

--- HotelRoom.java
+++ HotelRoom.java
@@ -9,33 +9,61 @@ public class HotelRoom{ // Defining a PUBLIC HotelRoom class
 
     }
 
-    void checkin(Person guest){
-        guestOne = guest;
-    }
+    void checkIn(Person guest){
+
+        while (number_guest <=2 ){
+            if (guestOne == null){
+                guestOne = guest;
+                number_guest = 1;
+            }
+            
+            if (guestOne!= null){
+                guestTwo = guest;
+                number_guest = 2;
+                }
+            
+            if (guestOne != null && guestTwo != null && number_guest == 2){
+                System.out.println("Maximum capacity reached.");
+                number_guest++;
+            }   
+        }
+            
+
+        }
 
     void checkOut(Person guest){
-        guestOne = null;
-    }
+        if (guestOne == guest){
+            guestOne = null;
+            number_guest = 0;
+        }
+        else{
+            guestTwo = null;
+            number_guest = 1;
+            }
+        }
 
     int checkCapacity(){
         if(guestOne != null && guestTwo != null){
             System.out.println("There are 2 guests.");
             return 2;
-        
         }
-        if (guestOne == null && guestTwo == null){
+        if(guestOne == null && guestTwo == null){
             System.out.println("There are 0 guests.");
             return 0;
-            
         }
-        if (guestOne == null || guestTwo == null){
+        if (guestOne == null && guestTwo != null){
             System.out.println("There is 1 guest");
             return 1;
-            
+        }
+
+        if (guestOne != null && guestTwo == null){
+            System.out.println("There is 1 guest");
+            return 1;
         }
         return 0;
 
     }
+
 
     void printGuestInfo(){
         System.out.println(guestOne.height);
